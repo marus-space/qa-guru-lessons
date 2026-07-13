1d6af4cd99c5827ae22b12f3659f59edf3f792ca
Maria Gopkalo <marus.space@gmail.com>
2026-07-13 20:44:59 +0300

    add env variables

---
+++ tests/2026-07-13-env.spec.js
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+import { UserBuilder } from '../src/helpers/builders';
+import { App } from '../src/pages';
+
+test('Пользователь может зарегистрироваться, используя email и пароль', async ({ page }) => {
+  const user = new UserBuilder().withEmail().withUsername().withPassword().build();
+  console.log('Переменные окружения');
+  console.log(process.env);
+  
+  const app = new App({ page });
+  
+  await app.mainPage.goToMainPage();
+  await app.mainPage.goToRegister();
+  await app.registerPage.signUp(user);
+
+  await expect(app.authenticatedMainPage.getProfileDropdown()).toContainText(user.username);
+});
